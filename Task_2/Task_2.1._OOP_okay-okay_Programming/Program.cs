d2a5c75275408e000f63d3b657acb908ab848e4c
Brainiak-inc <ilyadevwk@gmail.com>
2020-06-07 17:57:29 +0400

    Added string comparison method

--- Task_2/Task_2.1._OOP_okay-okay_Programming/Program.cs
+++ Task_2/Task_2.1._OOP_okay-okay_Programming/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task_2._1._OOP_okay_okay_Programming
@@ -10,7 +12,40 @@ namespace Task_2._1._OOP_okay_okay_Programming
     {
         static void Main(string[] args)
         {
+            string str_1, str_2;
+            Console.WriteLine("Enter string 1: ");
 
+            str_1 = Console.ReadLine();
+            Console.WriteLine("Enter string 2: ");
+
+            str_2 = Console.ReadLine();
+
+            Console.WriteLine(myString.StringComparison(str_1, str_2));
+
+
+        }    
+    }
+    class myString
+    {
+        public static bool StringComparison(string str1, string str2)
+        {
+
+            char[] charArray1 = str1.ToCharArray();
+            char[] charArray2 = str2.ToCharArray();
+
+            if (charArray1.Length != charArray2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < charArray1.Length; i++)
+            {
+                if (charArray1[i] != charArray2[i])
+                {
+                    return false;
+                }
+                
+            }
+            return true;
         }
     }
 }

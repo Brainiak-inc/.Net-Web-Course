fe6a2932167469ab882196504774960181d6510c
Brainiak-inc <ilyadevkw@gmail.com>
2020-06-07 21:08:47 +0400

    Added search methods

--- Task_2/Task_2.1._OOP_okay-okay_Programming/Program.cs
+++ Task_2/Task_2.1._OOP_okay-okay_Programming/Program.cs
@@ -12,16 +12,25 @@ namespace Task_2._1._OOP_okay_okay_Programming
     {
         static void Main(string[] args)
         {
+            //Comparison
             string str_1, str_2;
-            Console.WriteLine("Enter string 1: ");
 
+            Console.Write("Enter string 1: ");
             str_1 = Console.ReadLine();
-            Console.WriteLine("Enter string 2: ");
 
+            Console.Write("Enter string 2: ");
             str_2 = Console.ReadLine();
 
             Console.WriteLine(myString.StringComparison(str_1, str_2));
 
+            //Symbol searching 
+            Console.Write("Enter new sentence: \n");
+            string sentence = Console.ReadLine();
+
+            Console.Write("Enter only one symbol, which you want to find in your sentence: ");
+            char symbol = char.Parse(Console.ReadLine());
+            Console.WriteLine($"Here's your sybmbol: {myString.CharSearch(sentence, symbol)}");
+            Console.WriteLine($"Index of your symbol: {myString.IndexOf(sentence, symbol)}");
 
         }    
     }
@@ -46,6 +55,53 @@ namespace Task_2._1._OOP_okay_okay_Programming
                 
             }
             return true;
+        }
+        //public static string ConcatString(string String1, string String2)
+        //{
+        //    string resultString;
+        //    char[] charArray1 = String1.ToCharArray();
+        //    char[] charArray2 = String2.ToCharArray();
+
+
+
+        //}
+        public static char CharSearch(string String, char symbol)
+        {
+            char ch = '0';
+            char[] charArray = String.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if(charArray[i] == symbol)
+                {
+                    ch = symbol;
+                    break;
+                }
+                else
+                {
+                    ch = '0';
+                }
+            }
+           
+            return ch;
+        }
+
+        //I decided, that i have to search not just symbol, but it's index too
+        //so here we go!
+        public static int IndexOf(string String, char symbol)
+        {
+            int indexOfSymbol = 0;
+            char[] charArray = String.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (charArray[i] == symbol)
+                {
+                    indexOfSymbol = i;
+                    break;
+                }
+            }
+            return indexOfSymbol;
         }
     }
 }

da2d3a891cbd041f67fe30bd6a078f76a2c9d305
Brainiak-inc <ilyadevkw@gmail.com>
2020-06-10 19:47:24 +0400

    Added custom paint

---
+++ Task_2/Task_2.1.2_Custom_Paint/Task_2.1.2_Custom_Paint/Task_2.1.2_Custom_Paint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._1._2_Custom_Paint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

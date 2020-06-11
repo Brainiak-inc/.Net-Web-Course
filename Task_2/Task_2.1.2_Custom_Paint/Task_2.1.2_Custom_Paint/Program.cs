ca683409c211a77d021289debf7b7f7b5c9e5ebf
Brainiak-inc <ilyadevwk@gmail.com>
2020-06-11 15:27:12 +0400

    Some fixes

--- Task_2/Task_2.1.2_Custom_Paint/Task_2.1.2_Custom_Paint/Program.cs
+++ Task_2/Task_2.1.2_Custom_Paint/Task_2.1.2_Custom_Paint/Program.cs
@@ -12,4 +12,26 @@ namespace Task_2._1._2_Custom_Paint
         {
         }
     }
+    public class Ring
+    {
+        protected double radius;
+        protected double x;
+        protected double y;
+        public Ring(double Radius, double X, double Y)//Constrctor with 3 params
+        {
+            x = X;
+            y = Y;
+            radius = Radius;
+        }
+        public Ring(double Radius)
+        {
+            x = 0;
+            y = 0;
+            radius = Radius;
+        }
+        public void DisplayRing()
+        {
+
+        }
+    }
 }

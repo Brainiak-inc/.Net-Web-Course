6f226f39c975a26948fad2ba533d67ca10330ed4
Brainiak-inc <ilyadevkw@gmail.com>
2020-06-16 22:41:04 +0400

    Some fixes

--- Task_2/Task_2.1.2_Custom_Paint/Task_2.1.2_Custom_Paint/Program.cs
+++ Task_2/Task_2.1.2_Custom_Paint/Task_2.1.2_Custom_Paint/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq;  
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_2._1._2_Custom_Paint
 {
@@ -12,26 +11,34 @@ namespace Task_2._1._2_Custom_Paint
         {
         }
     }
-    public class Ring
+    public class Dot
     {
-        protected double radius;
-        protected double x;
-        protected double y;
-        public Ring(double Radius, double X, double Y)//Constrctor with 3 params
-        {
-            x = X;
-            y = Y;
-            radius = Radius;
-        }
-        public Ring(double Radius)
-        {
-            x = 0;
-            y = 0;
-            radius = Radius;
-        }
-        public void DisplayRing()
+        private int x, y;
+
+        void getCoordinates(int x, int y)
         {
 
         }
+        public Dot()
+        {
+            Console.Write($"Enter coordinate x: {x}");
+            x = int.Parse(Console.ReadLine());
+            Console.Write($"Enter coordinate y: {y}");
+            y = int.Parse(Console.ReadLine());
+
+        }
+        protected virtual void SetDot()
+        {
+            Console.SetCursorPosition(x,y);
+        }
+    }
+    public class Ring : Dot
+    {
+        private int radius;
+        public void PrintRing()
+        {
+            
+        }
+
     }
 }

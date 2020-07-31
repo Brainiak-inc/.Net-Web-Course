75ca6d10fa2aaff4661a97a679564a596bf70967
Brainiak-inc <ilyadevwk@gmail.com>
2020-07-31 15:18:57 +0400

    Modified backuping class

--- Task_4/Task_4_1/NewGitFilesApp/utils/Backup.cs
+++ Task_4/Task_4_1/NewGitFilesApp/utils/Backup.cs
@@ -5,11 +5,57 @@ using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using System.Runtime.Remoting.Messaging;
+using System.Xml;
+using System.Globalization;
 
 namespace NewGitFilesApp.utils
 {
     class Backup
     {
-        
+        private static Dictionary<int, Tuple<string, DateTime, string, string>> _dict = new Dictionary<int, Tuple<string, DateTime, string, string>>();
+
+        private static void XmlInfo()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(@"D:\TestsApps\TestFolder"); //Change this directory
+
+            XmlNodeList noteList = xmlDoc.GetElementsByTagName("Note");
+            foreach (XmlNode note in noteList)
+            {
+                int id = Int32.Parse(note["ID"].InnerText);
+                string name = note["Name"].InnerText;
+                DateTime date = DateTime.ParseExact(note["LastWritten"].InnerText, "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                string path = note["Full path"].InnerText;
+                string text = note["Text"].InnerText;
+                _dict.Add(id, new Tuple<string, DateTime, string, string>(name, date, path, text));
+
+            }
+        }
+        public static void DisplayXmlInfo()
+        {
+            XmlInfo();
+            for (int i = 0; i < _dict.Count; i++)
+            {
+                var value = _dict[i + 1];
+                Console.WriteLine($"\nNote {_dict.Keys.ElementAt(i)}. \nFilse's name: {value.Item1}. " +
+                    $"Changed: {value.Item2}. Path {value.Item3}. Inner text: \n{value.Item4}");
+            }
+        }
+        public static bool ChekDate(DateTime inputDate)
+        {
+            bool chek = false;
+            for (int i = 0; i < _dict.Count; i++)
+            {
+                if (_dict[i + 1].Item2.Equals(inputDate))
+                {
+                    chek = true;
+                }
+            }
+            return chek;
+        }
+        public static void BackUpMaker()
+        {
+
+        }
     }
 }

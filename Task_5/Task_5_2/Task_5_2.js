c579e97499e044167481e6c8f4437e4ea6f3eaaa
Brainiak-inc <ilyadevkw@gmail.com>
2020-08-16 12:32:26 +0400

    Added completed Task 5.2

--- Task_5/Task_5_2/Task_5_2.js
+++ Task_5/Task_5_2/Task_5_2.js
@@ -20,5 +20,34 @@ function Calculator(input) {
 	input = input.replace(/\s/g, '');
 
 	let output;
-	
+
+	for(let i = 0, n = operations.operationsOrder.length; i < n; i++){
+		let calculation = new RegExp('(\\d+\\.?\\d*)([\\' + operations.operationsOrder[i].join('\\') + '])(\\d+\\.?\\d*)');
+		calculation.lastIndex = 0;
+		while(calculation.test(input)){
+			output = _calculate(RegExp.$1, RegExp.$2, RegExp.$3);
+			if (isNaN(output) || !isFinite(output)) {
+				return output;
+			} 
+			input = input.replace(calculation, output);
+		}
+	}
+	return output;
+
+	function _calculate(a, operation, b) {
+		a = a * 1;
+		b = b * 1;
+		switch(operation){
+			case operations.addition: return a + b;
+		    break;
+		    case operations.substraction: return a - b;
+		    break;
+		    case operations.multiplication: return a * b;
+		    break;
+		    case operations.substraction: return a / b;
+		    break;
+		    default: null;
+		}
+	}
 }
+
